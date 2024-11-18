aa91b9a12c76d8c11d10777e78a5e4f5ba252db5
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-17 20:43:48 -0500

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using LiamellCruz_Ap1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiamellCruz_Ap1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+
+    public DbSet<Registro> Registro { get; set; }
+}
