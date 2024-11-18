ecddaeee2eaa26496469ce80fe4b0fbcaf414c4e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-17 20:51:10 -0500

    Migracion

---
+++ Migrations/20241118001649_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiamellCruz_Ap1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registro",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MyProperty1 = table.Column<int>(type: "int", nullable: false),
+                    MyProperty2 = table.Column<int>(type: "int", nullable: false),
+                    MyProperty3 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registro", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registro");
+        }
+    }
+}
