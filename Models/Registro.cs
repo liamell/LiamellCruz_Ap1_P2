7614fdfba07abcd52160797adf44ca68d7572367
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-17 20:42:16 -0500

    Modelo de ejemplo

---
+++ Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiamellCruz_Ap1_P2.Models;
+
+public class Registro
+{
+    [Key]
+    public int id{ get; set; }
+    public int MyProperty1 { get; set; }
+    public int MyProperty2 { get; set; }
+    public int MyProperty3 { get; set; }
+}
