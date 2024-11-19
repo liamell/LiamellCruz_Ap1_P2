2d964b0d67a3663ce27827615e4c7ad40fb1f7b5
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-18 19:49:31 -0500

    Modelo Combo

---
+++ Models/Combo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiamellCruz_Ap1_P2.Models;
+
+public class Combo
+{
+
+    [Key]
+    public int ComboId{ get; set; }
+
+    [Required(ErrorMessage = " Campo obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = " Campo obligatorio")]
+    [RegularExpression(@"[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = " Campo obligatorio")]
+    public int Precio { get; set; }
+
+
+}
