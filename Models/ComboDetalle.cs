e13961ba818f732ebcf8a63bf745d9b4f51ef7f6
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-18 19:53:10 -0500

    Create

---
+++ Models/ComboDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiamellCruz_Ap1_P2.Models;
+
+public class ComboDetalle
+{
+    [Key]
+
+    public int ComboDetalleId { get; set; }
+    public int ComboId { get; set; }
+    public int ArticuloId { get; set; }
+    public int Cantidad { get; set; }
+
+
+}
