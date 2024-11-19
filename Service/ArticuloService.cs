6a1205124f689cfd2b16689b4be0931ca583049a
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-11-18 20:57:26 -0500

    Articulo Service

---
+++ Service/ArticuloService.cs
@@ -0,0 +1,19 @@
+﻿using LiamellCruz_Ap1_P2.DAL;
+using LiamellCruz_Ap1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace LiamellCruz_Ap1_P2.Service;
+
+public class ArticuloService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<Articulo>> Listar(Expression<Func<Articulo, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Articulo
+        .AsNoTracking()
+        .Where(criterio)
+        .ToListAsync();
+    }
+
+}
